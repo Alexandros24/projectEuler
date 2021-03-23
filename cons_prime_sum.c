db59c1925fa0defba9d0a135323fa7fd01d54127
Alexandros24 <alexanderaska@gmail.com>
2021-03-23 22:59:26 +0200

    v1.

--- cons_prime_sum.c
+++ cons_prime_sum.c
@@ -15,7 +15,7 @@ int isPrime(long n)
         return 1;
     for(int i = 2; i < sqrt(n) + 1; i++)
     {
-        if(n % i == 0;)
+        if(n % i == 0)
             return 0;
     }
     return 1;
