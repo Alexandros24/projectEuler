76d4a8f1c28ddd842999939e8a079fc9800b66da
Alexandros24 <alexanderaska@gmail.com>
2021-03-29 11:03:34 +0300

    made changes to problem

--- problem9.py
+++ problem9.py
@@ -3,7 +3,7 @@ import numpy
 def check_pathagorean(a, b, c):
     return a**2 + b**2 == c**2
 
-for a in range(1, 332): #after 332 x + y + z > 1000, becuase a < b < c
+for a in range(0, 332, 5): #after 332 x + y + z > 1000, becuase a < b < c
     for b in range(a+1, int(numpy.ceil((1000-a)/2.-1))):
         c = 1000 - a - b
         if (check_pathagorean(a, b, c)):
