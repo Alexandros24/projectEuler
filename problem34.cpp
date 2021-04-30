dd7fb2c434a9c300c2aff5289ee24a0bc54dbd85
Alexander <74398883+Alexandros24@users.noreply.github.com>
2021-04-30 14:23:34 +0300

    Commited Changes

---
+++ problem34.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+
+
+int main()
+{
+    // 7! > 1000 so biggest possible fact will be 6!
+
+    // initialize the sum
+    unsigned int sum = 0;
+
+    // 8 x 9! = 2903040 a  digit number, thus we know that all numbers will be <= limit.
+    unsigned int limit = 2903040; 
+
+    //create a cache since we know the factorial of number 1-6.
+    int cache[10] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+
+    for(int i = 10; i < limit; i++) // above 1-digit numbers
+    {
+        unsigned int tsum = 0; // the sum of the split fact numbers.
+
+        // split the number to its digits 
+        unsigned int temp = i;
+        while(temp > 0)
+        {
+            // add the factorial of the split num
+            tsum += cache[temp % 10];
+
+            // remove the digit from the number
+            temp /= 10;
+        }
+        // check if tsum is equal to the number
+        if(tsum == i)
+            sum += i;
+    }
+    std::cout << "The sum is equal to: " << sum << std::endl;
+}
